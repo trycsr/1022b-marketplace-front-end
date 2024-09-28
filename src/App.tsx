26aebb7f9029f87a6be60c4cf2d71cc9962d7985
Ryan Christian SR <ryancristiansilva551@gmail.com>
2024-09-28 08:24:51 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Ryan Cristian comedor de xereka</h1>
+      <h1>Ryan Cristian XDDDD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
