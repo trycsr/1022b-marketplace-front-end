806cb5c7fbef97b83787b2aba20bf521ba1d6eb7
Ryan Christian SR <ryancristiansilva551@gmail.com>
2024-11-01 08:03:05 -0400

    mudei import e afins

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,74 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 type ProdutoType = {
-  id: number;
-  nome: string;
-  descricao: string;
-  preco: string;
-  imagem: string;
-};
-
-//Criamos um tipo usuario para classificar o tipo da const e especificar a tipagem dos dados que foram inseridos no banco e serão mostrados na tela
+  id:number,
+  nome:string,
+  descricao:string,
+  preco:string,
+  imagem:string
+}
 type UsuarioType = {
-  id: number;
-  nome: string;
-  email: string;
-  created_at: string;
-  updated_at: string;
-};
-
+  id:number,
+  name:string,
+  email:string,
+  created_at:string,
+  update_at:string 
+}
 function App() {
-  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
-  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
+  const [produtos, setProdutos] = useState<ProdutoType[]> ([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]> ([])
 
-  useEffect(() => {
-    console.log("Carregando produtos...");
+//useEffect(oque fazer e quando fazer)
+  useEffect(()=>{
     fetch("https://one022b-marketplace-w15n.onrender.com/produtos")
-      .then(resposta => resposta.json())
-      .then(dados => setProdutos(dados))
-      .catch(error => console.error("Erro ao buscar produtos:", error));
-  }, []);
-//Criamos um UseEffect para colocar a url do usuario e retornar seus dados
-  useEffect(() => {
-    console.log("Carregando usuários...");
+    .then(resposta=>resposta.json())
+    .then(dados=>setProdutos(dados))
+  },[])
+  useEffect(()=>{
     fetch("https://one022b-marketplace-w15n.onrender.com/usuarios")
-      .then(resposta => resposta.json())
-      .then(dados => setUsuarios(dados))
-      .catch(error => console.error("Erro ao buscar usuários:", error));
-  }, []);
+    .then(resposta=>resposta.json())
+    .then(dados=>setUsuarios(dados))
+  },[])
 
+  
   return (
     <>
-      <div className='container-produtos'>
-        {produtos.map(prod => (
-          <div key={prod.id} className='produto-item'>
-            <h1>{prod.nome}</h1>
-            <img src={prod.imagem}/>
-            <p>{prod.descricao}</p>
-            <p>{prod.preco}</p>
+     <h2 className='titulo'>Produtos</h2>
+    <div className='container-produtos'>
+      {
+        produtos.map(prod=>{
+          return(
+            <div key={prod.id} className='produto-item'>
+              <h1>
+                {prod.nome}
+              </h1>
+              <img src={prod.imagem} alt='Imagem do Produto'/>
+              <p>{prod.preco}</p>
+              <p>{prod.descricao}</p>
+            </div> 
+          )
+        })
+      }
+       </div>
 
-          </div>
-        ))}
-      </div>
-      
-      <div className='container-usuarios'>
-        {usuarios.map(usua => (
-          <div key={usua.id} className='usuario-item'>
-            <h1>{usua.nome}</h1>
-            <p>{usua.email}</p>
-            <p>{usua.created_at}</p>
-            <p>{usua.updated_at}</p>
-          </div>
-        ))}
-      </div>
+       <h2 className='titulo'>Usuários</h2>
+       <div className='container-usuarios'>
+      {usuarios.map(user=>{
+          return(
+            <div key={user.id} className='usuario-item'>
+              <h1>{user.name}</h1>
+              <p>{user.email}</p>
+              <p>{user.created_at}</p>
+              <p>{user.update_at}</p>
+            </div>
+          )
+        })
+      }
+       </div>
+       
     </>
-    /Criamos uma div para conter os dados da tabela usuarios do banco de dados/ 
-  );
+  )
 }
 
-export default App;
+export default App
